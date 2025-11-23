44bb3393b6648251c9b481a4b9380018b1613245
atsoy <anatoli.tsoi@strawberry.se>
2025-11-23 12:23:58 +0100

    initialized terraform/provision

--- terraform/provision/s3/main.tf
+++ terraform/provision/s3/main.tf
@@ -1,8 +1,9 @@
 variable "service_name" { type = string }
 variable "environment" { type = string }
+variable "aws_region" {type = string }
 variable "tags" { type = map(string) }
 
-locals { bucket_name = "${var.service_name}-${var.environment}" }
+locals { bucket_name = "${var.service_name}-${var.environment}-${var.aws_region}" }
 
 output "bucket_name" { value = aws_s3_bucket.app_bucket.bucket }
 output "bucket_arn"  { value = aws_s3_bucket.app_bucket.arn }
